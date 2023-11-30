22feb10fca1160aa7dca0b00235aff0a0f807dfc
Kevin-Ezer F. Tanierla <81379022+StainedRabbit@users.noreply.github.com>
2023-12-01 07:31:17 +0800

    Delete portfolio/local_settings.py

--- portfolio/local_settings.py
+++
@@ -1,6 +0,0 @@
-DATABASE_PASSWORD = "admin"
-DATABASE_PORT = 5432
-DATABASE_NAME = "portfolio_2"
-DATABASE_USER = "postgres"
-DATABASE_ENGINE = "django.db.backends.postgresql"
-DATABASE_HOST = "localhost"
